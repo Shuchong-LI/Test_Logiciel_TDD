a2b0c56bcf15b8e7d996b4ea10b99638f5c95628
xxz-666 <lsc13361272744@163.com>
2023-01-27 15:23:25 +0100

    [ex1] add test for isSuiteArethmetique()

--- test_ex1.py
+++ test_ex1.py
@@ -22,6 +22,26 @@ class test_ex1(unittest.TestCase):
 		self.assertEqual(ex1.isSuiteGeometrique([1,2,'a','b','c']),False)
 		self.assertEqual(ex1.isSuiteGeometrique([2,4]),False)
 
+	def test_isSuiteArethmetique(self):
+		print("testing isSuiteArethmetique...")
+
+		#integer list
+		self.assertEqual(ex1.isSuiteArethmetique([0,0,0,0,0]),True)
+		self.assertEqual(ex1.isSuiteArethmetique([1,2,3,4,5]),True)
+		self.assertEqual(ex1.isSuiteArethmetique([2,4,6,8,10]),True)
+		self.assertEqual(ex1.isSuiteArethmetique([3,9,15,21]),True)
+		self.assertEqual(ex1.isSuiteArethmetique([4,20,36,52]),True)
+
+		#float list 
+		self.assertEqual(ex1.isSuiteArethmetique([1.2,2.4,3.6,4.8]),True)
+		self.assertEqual(ex1.isSuiteArethmetique([2.5,5,7.5.10,12.5]),True)
+
+		#fault test
+		self.assertEqual(ex1.isSuiteArethmetique("I am not a list"),False)
+		self.assertEqual(ex1.isSuiteArethmetique([1,2,3,4,7]),False)
+		self.assertEqual(ex1.isSuiteArethmetique([1,2,'a','b','c']),False)
+
+
 
 
 
