7ec9bfab75025f8f2ef3445e5ad056a6bd8680bd
xxz-666 <lsc13361272744@163.com>
2023-01-31 20:47:15 +0100

    [ex1] add test for A_n_suivant()

--- test_ex1.py
+++ test_ex1.py
@@ -61,25 +61,22 @@ class test_ex1(unittest.TestCase):
 
 
 
-	# def test_A_n_suivant(self):
-	# 	print("testing isSuiteArethmetique...")
+	def test_A_n_suivant(self):
+		print("testing A_n_suivant...")
 
-	# 	#integer list
-	# 	self.assertEqual(ex1.isSuiteArethmetique([0,0,0,0,0]),True)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([1,2,3,4,5]),True)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([2,4,6,8,10]),True)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([3,9,15,21]),True)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([4,20,36,52]),True)
+		#integer list
+		self.assertEqual(ex1.A_n_suivant(3,[0,0,0,0,0]),(True,[0,0,0]))
+		self.assertEqual(ex1.A_n_suivant(3,[1,2,3,4,5]),(True,[6,7,8]))
+		self.assertEqual(ex1.A_n_suivant(2,[2,4,6,8,10]),(True,[12,14]))
+		self.assertEqual(ex1.A_n_suivant(4,[3,9,15,21]),(True,[27,33,39,45]))
+		self.assertEqual(ex1.A_n_suivant(1,[4,20,36,52]),(True,[68]))
 
-	# 	#float list 
-	# 	self.assertEqual(ex1.isSuiteArethmetique([2.5,5,7.5,10,12.5]),True)
-	# 	# self.assertEqual(ex1.isSuiteArethmetique([1.1,2.2,3.3,4.4]),True) # precision fault
 
-	# 	#fault test
-	# 	self.assertEqual(ex1.isSuiteArethmetique("I am not a list"),False)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([1,2,3,4,7]),False)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([1,2,'a','b','c']),False)
-	# 	self.assertEqual(ex1.isSuiteArethmetique([1,2]),False)
+		#fault test
+		self.assertEqual(ex1.A_n_suivant(2,"I am not a list"),False)
+		self.assertEqual(ex1.A_n_suivant("I am not an int",[1,2,4]),False)
+		self.assertEqual(ex1.A_n_suivant("I am not an int","I am not a list"),False)
+		self.assertEqual(ex1.A_n_suivant(3,[1,2,3,4,6]),False)
 
 
 
