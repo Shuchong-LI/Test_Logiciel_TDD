7e940e004bec87f5b53fe34e8a0f606671c7881d
xxz-666 <lsc13361272744@163.com>
2023-01-31 20:39:43 +0100

    [ex1] add function G_n_suivant()

--- ex1.py
+++ ex1.py
@@ -45,3 +45,19 @@ def isSuiteArethmetique(l):
 			return False
 
 	return True
+
+def G_n_suivant(n_suiv,l):
+	# The first input should be an int!
+	if not isinstance(n_suiv,int) :
+		return False
+	# The second input should be a iist!
+	if not isSuiteGeometrique(l):
+		return False
+
+	l_suiv = list()
+
+	for i in range(n_suiv):
+		l_suiv.append((l[1]/l[0])**(i+1)*l[-1])
+
+
+	return True,l_suiv
