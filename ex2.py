db624a0fd3da78cd3d7b55246decf7ab7aa3b9ee
xxz-666 <lsc13361272744@163.com>
2023-02-02 16:26:52 +0100

    [ex2]add function verify_block()

--- ex2.py
+++ ex2.py
@@ -35,6 +35,10 @@ def verify_row(l):
 				l_flag[(l[i][j]-1)] = 1
 			else :
 				return False
+		
+		if l_flag != [1,1,1,1,1,1,1,1,1]:
+			return False
+		
 		l_flag = [0,0,0,0,0,0,0,0,0]
 
 	return True
@@ -52,6 +56,34 @@ def verify_column(l):
 				l_flag[(l[j][i]-1)] = 1
 			else :
 				return False
+
+		if l_flag != [1,1,1,1,1,1,1,1,1]:
+			return False
+
 		l_flag = [0,0,0,0,0,0,0,0,0]
+
+	return True
+
+def verify_block(l):
+	l_flag = [0,0,0,0,0,0,0,0,0]
+
+	if not verify_table_size(l) or not verify_element(l):
+		return False
+
+	for i in range(0,len(l),3):
+		for j in range(0,len(l[i]),3):
+			l_flag[(l[j][i]    -1)] += 1
+			l_flag[(l[j][i+1]  -1)] += 1
+			l_flag[(l[j][i+2]  -1)] += 1
+			l_flag[(l[j+1][i]  -1)] += 1
+			l_flag[(l[j+1][i+1]-1)] += 1
+			l_flag[(l[j+1][i+2]-1)] += 1
+			l_flag[(l[j+2][i]  -1)] += 1
+			l_flag[(l[j+2][i+1]-1)] += 1
+			l_flag[(l[j+2][i+2]-1)] += 1
+			if l_flag != [1,1,1,1,1,1,1,1,1]:
+				return False
+
+			l_flag = [0,0,0,0,0,0,0,0,0]
 
 	return True
