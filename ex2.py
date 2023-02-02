e7e7e50bc0cb54e931244b89ad05eb980e9688ae
xxz-666 <lsc13361272744@163.com>
2023-02-02 12:16:23 +0100

    [ex2]add function verify_row()

--- ex2.py
+++ ex2.py
@@ -19,5 +19,22 @@ def verify_element(l):
 		for j in range(len(l[i])):
 			if l[i][j] > 9 or l[i][j] < 1:
 				return False
-				
+
+	return True
+
+def verify_row(l):
+
+	l_flag = [0,0,0,0,0,0,0,0,0]
+
+	if not verify_table_size(l) or not verify_element(l):
+		return False
+
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			if l_flag[(l[i][j]-1)] == 0 :
+				l_flag[(l[i][j]-1)] = 1
+			else :
+				return False
+		l_flag = [0,0,0,0,0,0,0,0,0]
+
 	return True
