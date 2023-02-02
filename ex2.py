bb13541175da7518c551da29c39f573b50fbbf16
xxz-666 <lsc13361272744@163.com>
2023-02-02 14:48:57 +0100

    [ex2]add function verify_column()

--- ex2.py
+++ ex2.py
@@ -38,3 +38,20 @@ def verify_row(l):
 		l_flag = [0,0,0,0,0,0,0,0,0]
 
 	return True
+
+def verify_column(l):
+
+	l_flag = [0,0,0,0,0,0,0,0,0]
+
+	if not verify_table_size(l) or not verify_element(l):
+		return False
+
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			if l_flag[(l[j][i]-1)] == 0 :
+				l_flag[(l[j][i]-1)] = 1
+			else :
+				return False
+		l_flag = [0,0,0,0,0,0,0,0,0]
+
+	return True
