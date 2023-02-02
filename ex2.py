51510ec8d8bbf121072df5c665145d03403d700d
xxz-666 <lsc13361272744@163.com>
2023-02-02 10:31:55 +0100

    [ex2] add function verify_element()

--- ex2.py
+++ ex2.py
@@ -7,3 +7,17 @@ def verify_table_size(l):
 		if len(l[i]) != 9:
 			return False
 	return True
+
+def verify_element(l):
+
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			if not isinstance(l[i][j],int):
+				return False
+
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			if l[i][j] > 9 or l[i][j] < 1:
+				return False
+				
+	return True
