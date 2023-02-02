db22131e41887c8c85b2cbdbd58bd751db44ca21
xxz-666 <lsc13361272744@163.com>
2023-02-02 10:06:08 +0100

    add test for verify_table_size()

--- test_ex2.py
+++ test_ex2.py
@@ -1,8 +1,61 @@
 import ex2
 import unittest
 
+# 9x9
+l1 = 	[[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0]]
+
+# 8x9
+l2 = 	[[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0]]
+
+# 9x8
+l3 = 	[[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]]
+
+
+
 class test_ex2(unittest.TestCase):
-	def test_function_ex2(self):
-		print("testing function_ex2...")
-		self.assertEqual(ex2.function_ex2(),"hello ex2!")
-		self.assertNotEqual(ex2.function_ex2(),"Bye ex2!")		
+	#table 9x9
+	def test_verify_table_size(self):
+		print("testing verify_table_size...")
+
+		self.assertTrue(ex2.verify_table_size(l1))
+		self.assertFalse(ex2.verify_table_size(l2))
+		self.assertFalse(ex2.verify_table_size(l3))
+	#num 1-9
+	# def test_verify_int_range(self):
+
+		
+	# def test_verify_row(self):
+
+	# def test_verify_column(self):
+
+	# def test_verify_block(self):
+
+	# def test_verify_sudoku(self):
+	# 	print("testing verify_sudoku...")
+
+
+
+
